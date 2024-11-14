1e132fd35554343f0e4f1ddb09364516830cb035
Horvath Mark <brryy0371@gmail.com>
2024-11-14 16:47:07 +0100

    frissites

--- src/app/service-item/service-item.component.ts
+++ src/app/service-item/service-item.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, input, Output, output } from '@angular/core';
 import { Service } from '../models/service.model';
 
 @Component({
   selector: 'app-service-item',
   templateUrl: './service-item.component.html',
-  styleUrl: './service-item.component.css'
+  styleUrl: './service-item.component.css',
 })
 export class ServiceItemComponent {
-
   @Input() itemss!: Service;
+  @Output() selectionChanged = new EventEmitter<void>();
+
 }
